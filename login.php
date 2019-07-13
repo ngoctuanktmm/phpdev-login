cf191a9ae5186f776e191ae5fa6fffeaf262e51d
ngoctuan.ktmm <ngoctuan.ktmm@gmail.com>
2019-07-14 01:16:02 +0700

    Add files via upload

--- login.php
+++ login.php
@@ -1,70 +1,70 @@
 <?php
-// core configuration
-include_once "config/core.php";
- 
-// set page title
-$page_title = "Login";
- 
-// include login checker
-$require_login=false;
-include_once "login_checker.php";
- 
-// default to false
-$access_denied=false;
- 
-// post code will be here
-// include classes
-include_once "config/database.php";
-include_once "objects/user.php";
- 
-// get database connection
-$database = new Database();
-$db = $database->getConnection();
- 
-// initialize objects
-$user = new User($db);
- 
-// check if email and password are in the database
-$user->email=$_POST['email'];
- 
-// check if email exists, also get user details using this emailExists() method
-$email_exists = $user->emailExists();
- 
-// login validation will be here
-// validate login
-if ($email_exists && password_verify($_POST['password'], $user->password) && $user->status==1){
- 
-    // if it is, set the session value to true
-    $_SESSION['logged_in'] = true;
-    $_SESSION['user_id'] = $user->id;
-    $_SESSION['access_level'] = $user->access_level;
-    $_SESSION['firstname'] = htmlspecialchars($user->firstname, ENT_QUOTES, 'UTF-8') ;
-    $_SESSION['lastname'] = $user->lastname;
- 
-    // if access level is 'Admin', redirect to admin section
-    if($user->access_level=='Admin'){
-        header("Location: {$home_url}admin/index.php?action=login_success");
+    // core configuration
+    include_once "config/core.php";
+    
+    // set page title
+    $page_title = "Login";
+    
+    // include login checker
+    $require_login=false;
+    include_once "login_checker.php";
+    
+    // default to false
+    $access_denied=false;
+    
+    // post code will be here
+    // if the login form was submitted
+    if($_POST){
+        // email check will be here
+        // include classes
+        include_once "admin/config/database.php";
+        include_once "objects/user.php";
+        
+        // get database connection
+        $database = new Database();
+        $db = $database->getConnection();
+        
+        // initialize objects
+        $user = new User($db);
+        
+        // check if email and password are in the database
+        $user->email=$_POST['email'];
+        
+        // check if email exists, also get user details using this emailExists() method
+        $email_exists = $user->emailExists();
+        
+        // login validation will be here
+        // validate login
+        if ($email_exists && password_verify($_POST['password'], $user->password) && $user->status==1){
+        
+            // if it is, set the session value to true
+            $_SESSION['logged_in'] = true;
+            $_SESSION['user_id'] = $user->id;
+            $_SESSION['access_level'] = $user->access_level;
+            $_SESSION['firstname'] = htmlspecialchars($user->firstname, ENT_QUOTES, 'UTF-8') ;
+            $_SESSION['lastname'] = $user->lastname;
+        
+            // if access level is 'Admin', redirect to admin section
+            if($user->access_level=='Admin'){
+                header("Location: {$home_url}admin/index.php?action=login_success");
+            }
+        
+            // else, redirect only to 'Customer' section
+            else{
+                header("Location: {$home_url}index.php?action=login_success");
+            }
+        }
+        
+        // if username does not exist or password is wrong
+        else{
+            $access_denied=true;
+        }
     }
- 
-    // else, redirect only to 'Customer' section
-    else{
-        header("Location: {$home_url}index.php?action=login_success");
-    }
-} 
-// if username does not exist or password is wrong
-else{
-    $access_denied=true;
-}
-
-// if the login form was submitted
-if($_POST){
-    // email check will be here
-}
- 
-// login form html will be here
-// include page header HTML
-include_once "layout_head.php";
- 
+            
+    // login form html will be here
+    // include page header HTML
+    include_once "layout_head.php";
+    
     echo "<div class='col-sm-6 col-md-4 col-md-offset-4'>";
     
         // alert messages will be here
@@ -97,7 +97,7 @@ include_once "layout_head.php";
                 Your username or password maybe incorrect
             </div>";
         }
-            
+    
         // actual HTML login form
         echo "<div class='account-wall'>";
             echo "<div id='my-tab-content' class='tab-content'>";
@@ -116,7 +116,7 @@ include_once "layout_head.php";
         echo "</div>";
     
     echo "</div>";
- 
-// footer HTML and JavaScript codes
-include_once "layout_foot.php";
+    
+    // footer HTML and JavaScript codes
+    include_once "layout_foot.php";
 ?>
